259a99a40890f34630efac3e67b21ebd3f54a649
AyoAwodeyi <117488435+AyoAwodeyi@users.noreply.github.com>
2026-02-21 11:48:29 -0600

    intial backend push

---
+++ masconsulting/src/main/java/hackslu/masconsulting/MasconsultingApplication.java
@@ -0,0 +1,13 @@
+package hackslu.masconsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasconsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasconsultingApplication.class, args);
+	}
+
+}
